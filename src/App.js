99e2c391d958349d5b320c9c71f4ad24722a4d39
Jacob Reese <jacobreese123@gmail.com>
2022-05-21 18:07:43 -0400

    add card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import styles from "./App.module.css";
 import Header from "./components/Header";
+import Card from "./ui/Card";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <Card>
+        <div>
+          <p>"Hola mis amigos"</p>
+        </div>
+      </Card>
     </div>
   );
 }
